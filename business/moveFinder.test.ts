dc31a6d911782868365d451b62fd4add5a5506bd
hossein <h.ebr141@outlook.com>
2024-08-30 18:40:27 +0330

    rook and knight moves

--- business/moveFinder.test.ts
+++ business/moveFinder.test.ts
@@ -21,6 +21,33 @@ describe("pawn", () => {
   })
 })
 
+describe("rook", () => {
+  test("a1 rook initial moves", () => {
+    const rookSqr = board[0]
+    let rookMoves = movesForPieceOnSqr(board, rookSqr)
+    expect(rookMoves).toHaveLength(0)
+
+    const blockingPawnSqr = findSqrAt(board, "a2")
+    const blockingPawnMoves = movesForPieceOnSqr(board, blockingPawnSqr)
+    blockingPawnMoves[1].piece = blockingPawnSqr.piece
+    blockingPawnSqr.piece = undefined
+
+    rookMoves = movesForPieceOnSqr(board, rookSqr)
+    expect(rookMoves).toHaveLength(2)
+  })
+
+  test("rook at the middle", () => {
+    const e5 = findSqrAt(board, "e5")
+
+    e5.piece = board[0].piece
+    board[0].piece = undefined
+
+    const moves = movesForPieceOnSqr(board, e5)
+
+    expect(moves).toHaveLength(11)
+  })
+})
+
 describe("bishop", () => {
   test("c1 bishop initial moves", () => {
     const bishopSqr = board[2]
@@ -45,5 +72,36 @@ describe("bishop", () => {
     const moves = movesForPieceOnSqr(board, e5)
 
     expect(moves).toHaveLength(8)
+  })
+})
+
+describe("knight", () => {
+  test("b1 knight initial moves", () => {
+    const knightSqr = board[1]
+    let knightMoves = movesForPieceOnSqr(board, knightSqr)
+    expect(knightMoves).toHaveLength(2)
+
+    const blockingPawnSqr = findSqrAt(board, "d2")
+    const blockingPawnMoves = movesForPieceOnSqr(board, blockingPawnSqr)
+    blockingPawnMoves[1].piece = blockingPawnSqr.piece
+    blockingPawnSqr.piece = undefined
+
+    knightMoves = movesForPieceOnSqr(board, knightSqr)
+    expect(knightMoves).toHaveLength(3)
+  })
+
+  test("knight at the middle", () => {
+    const e2 = findSqrAt(board, "e2")
+    movesForPieceOnSqr(board, e2)[0].piece = e2.piece
+    e2.piece = undefined
+
+    const b1 = board[1]
+    const d5 = findSqrAt(board, "d5")
+    d5.piece = b1.piece
+    b1.piece = undefined
+
+    const moves = movesForPieceOnSqr(board, d5)
+
+    expect(moves).toHaveLength(7)
   })
 })
--- business/moveFinder.ts
+++ business/moveFinder.ts
@@ -2,6 +2,8 @@ import { type Sqr } from "~/models/Square"
 import { type Board } from "~/models/Board"
 import { findPawnMoves } from "./pawn"
 import { findBishopMoves } from "./bishop"
+import { findRookMoves } from "./rook"
+import { findKnightMoves } from "./knight"
 
 export const movesForPieceOnSqr = (board: Board, sqr: Sqr): Sqr[] => {
   if (!sqr.piece) return []
@@ -9,6 +11,10 @@ export const movesForPieceOnSqr = (board: Board, sqr: Sqr): Sqr[] => {
   switch (sqr.piece?.type) {
     case "pawn":
       return findPawnMoves(board, sqr)
+    case "rook":
+      return findRookMoves(board, sqr)
+    case "knight":
+      return findKnightMoves(board, sqr)
     case "bishop":
       return findBishopMoves(board, sqr)
     default:
