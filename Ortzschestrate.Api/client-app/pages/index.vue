c4903956fde453ddf7c7338957de0b3339032983
hossein <h.ebr141@outlook.com>
2024-10-13 18:30:11 +0330

    restructure with the backend project

---
+++ Ortzschestrate.Api/client-app/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>
+        Welcome to ortzschestrate!
+    </h1>
+
+<h4>
+    Create an account to start playing.
+</h4>
+</template>
--- models/Piece.ts
+++
@@ -1,9 +0,0 @@
-export type Color = "white" | "black"
-
-export type PieceType = "king" | "queen" | "bishop" | "knight" | "rook" | "pawn"
-
-export type Piece = {
-  id: number
-  color: Color
-  type: PieceType
-}
